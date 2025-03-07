1ef0001eddbe2ad94e14cc999ff8aa01b6fed32a
jvcarpena <jvcarpena2@gmail.com>
2025-03-07 17:28:34 +0800

    Add a simple header component

--- blog_react/src/App.jsx
+++ blog_react/src/App.jsx
@@ -1,4 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import NewPost from "./pages/NewPost";
+import PostPage from "./pages/PostPage";
+import About from "./pages/About";
+import Missing from "./pages/Missing";
+import { Routes, Route, useNavigate } from "react-router-dom";
 
 function App() {
   return (
--- blog_react/src/main.jsx
+++ blog_react/src/main.jsx
@@ -1,8 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter, Route } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Route path="/" element={App} />
+    </BrowserRouter>
   </StrictMode>
 );
